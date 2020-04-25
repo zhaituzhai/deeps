4daf0de7672f85294cb7fdef75a46e20d775ec26
zhaituzhai <995132549@qq.com>
2020-04-25 22:43:47 +0800

    concurrency thread pool start

---
+++ concurrency/src/main/java/com/zhaojm/concurrency/f_pool/AaThreadPoolExecutor.java
@@ -0,0 +1,30 @@
+package com.zhaojm.concurrency.f_pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author zhaojm
+ * @date 2020/4/25 22:18
+ */
+public class AaThreadPoolExecutor {
+
+    private static ExecutorService executor = Executors.newFixedThreadPool(5);
+
+    public static void main(String[] args) {
+        /*
+         * 1、如果任务为null，异常
+         * 2、获取当前线程池状态 + 线程个数变量的组合
+         * 3、当前线程池中线程个数是否小于 corePoolSize，小于则开启新线程运行
+         * 4、如果线程池处于 Running 状态，则添加任务到阻塞队列
+         * 4.1、二次检查
+         * 4.2、如果当前线程池状态不是 Running 则从队列中删除任务，并执行拒绝策略
+         * 4.3、否则如果当前线程池为空，则添加一个线程
+         * 5、如果队列满，则新增线程，新增失败则执行拒绝策略
+         */
+        executor.execute(() -> {
+            System.out.println("hello thread pool!");
+        });
+    }
+
+}
---
+++ concurrency/src/main/java/com/zhaojm/concurrency/f_pool/a_ThreadPool.md
@@ -0,0 +1,15 @@
+## 线程池
+
+线程池主要解决两个问题：\
+1、执行大量异步线程时线程池能够提供较好的性能，创建与销毁线程时需要开销的，线程池的线程是可复用的。\
+2、线程池提供了一种资源限制和管理手段，比如限制线程个数，动态新增线程。\
+
+另外线程池提供可调参数和可扩展性接口，满足不同情景
+
+newCachedThreadPool (线程池线程可用个数可达 Integer.MAX_VALUE)
+newFixedThreadPool (固定大小的线程池)
+newSingleThreadExecutor (单个线程池，单排队的线程可达 Integer.MAX_VALUE)
+
+Executors 是一个工具类，提供不同线程池实例的静态方法。
+
+ThreadPoolExecutor 继承了 AbstractExecutorService 成员变量 ctl 是一个 integer 的原子变量。用来记录线程池状态和线程池中线程个数
