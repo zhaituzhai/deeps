d7b36d4d8097c950b858d6d4f97499d5e24094f6
mattezhao <995132549@qq.com>
2022-03-06 00:35:58 +0800

    leetcode 刷题

---
+++ algorithm/src/main/java/com/zhaojm/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,16 @@
+package com.zhaojm.leetcode.editor.cn;
+
+/**
+ * 树
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
